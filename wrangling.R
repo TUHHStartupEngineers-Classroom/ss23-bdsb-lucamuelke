153683aa12db456662fc6e44848ccbb9d5a2d9c7
Luca Mülke <67710173+lucamuelke@users.noreply.github.com>
2023-05-22 13:19:22 +0200

    Data

---
+++ wrangling.R
@@ -0,0 +1,38 @@
+library(vroom)
+col_types <- list(
+  id = col_character(),
+  type = col_character(),
+  number = col_character(),
+  country = col_character(),
+  date = col_date("%Y-%m-%d"),
+  abstract = col_character(),
+  title = col_character(),
+  kind = col_character(),
+  num_claims = col_double(),
+  filename = col_character(),
+  withdrawn = col_double()
+)
+
+patent_tbl <- vroom(
+  file       = "patent.tsv", 
+  delim      = "\t", 
+  col_types  = col_types,
+  na         = c("", "NA", "NULL")
+)
+
+assignee_tbl <- vroom(
+  file       = "assignee.tsv", 
+  delim      = "\t", 
+  col_types  = col_types,
+  na         = c("", "NA", "NULL")
+)
+
+patent_assignee_tbl <- vroom(
+  file       = "patent_assignee.tsv", 
+  delim      = "\t", 
+  col_types  = col_types,
+  na         = c("", "NA", "NULL")
+)
+
+most_common_id <- sort(table(patent_assignee_tbl$assignee_id), decreasing=TRUE)[1]
+most_common_name <- assignee_tbl[id == most_common_id]
